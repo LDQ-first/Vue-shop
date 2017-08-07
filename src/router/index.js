21b175f42c9c6a19f93de1cc2d9fb385018d0c92
LDQ-first <178848890@qq.com>
2017-08-08 00:08:08 +0800

    'modify'

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodList from '../views/GoodList'
-import Title from '@/views/Title'
-import Image from '@/views/Image'
 import Cart from '@/views/Cart'
 
 Vue.use(Router)
--- src/views/GoodList.vue
+++ src/views/GoodList.vue
@@ -10,18 +10,18 @@
             <span class="sortby">Sort by:</span>
             <a href="javascript:void(0)" class="default cur">Default</a>
             <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-            <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+            <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
           </div>
           <div class="accessory-result">
             <!-- filter -->
-            <div class="filter stopPop" id="filter">
+            <div class="filter stopPop" id="filter" :class="{'filterby-show': filterBy}">
               <dl class="filter-price">
                 <dt>Price:</dt>
-                <dd><a href="javascript:void(0)">All</a></dd>
-                <dd>
-                  <a href="javascript:void(0)">0 - 100</a>
+                <dd @click="setPriceFilter('all')" ><a href="javascript:void(0)" :class="{'cur':priceChecked === 'all'}">All</a></dd>
+                <dd v-for="(price, index) in priceFilter" key="index" @click="setPriceFilter(index)"  >
+                  <a href="javascript:void(0)" :class="{'cur': priceChecked == index}">{{price.startPrice}} - {{price.endPrice}}</a>
                 </dd>
-                <dd>
+                <!--<dd>
                   <a href="javascript:void(0)">100 - 500</a>
                 </dd>
                 <dd>
@@ -29,7 +29,7 @@
                 </dd>
                 <dd>
                   <a href="javascript:void(0)">1000 - 2000</a>
-                </dd>
+                </dd>-->
               </dl>
             </div>
 
@@ -37,19 +37,19 @@
             <div class="accessory-list-wrap">
               <div class="accessory-list col-4">
                 <ul>
-                  <li>
+                  <li v-for="(item, index) in goodsList" key="index">
                     <div class="pic">
-                      <a href="#"><img src="static/1.jpg" alt=""></a>
+                      <a href="#"><img v-lazy="`static/${item.prodcutImg}`" alt=""></a>
                     </div>
                     <div class="main">
-                      <div class="name">XX</div>
-                      <div class="price">999</div>
+                      <div class="name">{{item.productName}}</div>
+                      <div class="price">{{item.prodcutPrice}}</div>
                       <div class="btn-area">
                         <a href="javascript:;" class="btn btn--m">加入购物车</a>
                       </div>
                     </div>
                   </li>
-                  <li>
+                  <!--<li>
                     <div class="pic">
                       <a href="#"><img src="static/2.jpg" alt=""></a>
                     </div>
@@ -84,13 +84,14 @@
                         <a href="javascript:;" class="btn btn--m">加入购物车</a>
                       </div>
                     </div>
-                  </li>
+                  </li>-->
                 </ul>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
      <NavFooter/>
     </div>
 </template>
@@ -101,9 +102,28 @@
     import NavHeader from '@/components/Header'
     import NavFooter from '@/components/Footer'
     import Bread from '@/components/Bread'
+    import axios from 'axios'
     export default{
         data(){
             return {
+                goodsList: [],
+                priceFilter: [
+                    {
+                        startPrice: '0.00',
+                        endPrice: '500.00'
+                    },
+                    {
+                        startPrice: '500.00',
+                        endPrice: '1000.00'
+                    },
+                    {
+                        startPrice: '1000.00',
+                        endPrice: '2000.00'
+                    }
+                ],
+                priceChecked: 'all',
+                filterBy: false,
+                overLayFlag: false
 
             }
         },
@@ -111,6 +131,29 @@
             NavHeader,
             NavFooter,
             Bread
+        },
+        mounted: function () {
+            this.getGoodsList()
+        },
+        methods: {
+            getGoodsList() {
+                axios.get("/goods").then(result => {
+                    var res = result.data.data
+                    this.goodsList = res
+                })
+            },
+            showFilterPop() {
+                this.filterBy = true
+                this.overLayFlag = true
+            },
+            closePop() {
+                this.filterBy = false
+                this.overLayFlag = false
+            },
+            setPriceFilter(state) {
+                this.priceChecked = state
+                this.closePop()
+            }
         }
     }
 </script>
