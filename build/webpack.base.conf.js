c0681a44fd0cf4c1c86911f5f1f71dd25444a4db
LDQ-first <178848890@qq.com>
2017-08-27 17:21:40 +0800

    'modify'

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+const webpack = require('webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -62,5 +63,18 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      async: 'shared-module',
+      minChunks: (module, count) => (
+        count >= 2    // 当一个模块被重复引用2次或以上的时候单独打包起来。 
+      )
+    }),
+    //这个plugin是用于引入dll里生成的json的。
+    new webpack.DllReferencePlugin({
+      context: __dirname,
+      manifest: require('../static/public/js/vendor-mainfest.json')  // 指向这个json
+    })
+  ]
 }
---
+++ build/webpack.dll.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+    entry: {
+        vendor: ['vue/dist/vue.esm.js', 'vue-router', 'vuex', 'axios', 
+        'vue-lazyload', 'echarts', 'vue-infinite-scroll']
+    },
+    output: {
+        path: path.join(__dirname, '../static/public/js'),
+        filename: '[name].dll.js',  // 输出的文件，将会根据entry命名为vendor.dll.js
+        library: '[name]_library'   // 暴露出的全局变量名
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            //path 是 manifest.json 文件的输出路径，这个文件会用于后续的业务代码打包
+            path: path.join(__dirname, '../static/public/js/', '[name]-mainfest.json'), // 描述依赖对应关系的json文件
+            //是 dll 暴露的对象名，要跟 output.library 保持一致
+            name: '[name]_library',
+             //context 是解析包路径的上下文，这个要跟接下来配置的 webpack.config.js 一致, 对之后DllReferencePlugin有用
+            context: __dirname
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+}
+
